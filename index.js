2d2cb0aab07bfd9898eb2dcd2a4e9bc43435f0ec
Mohammed Roshdy <mohammed.roshdy@cobrowser.net>
2026-07-22 02:48:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        environment: process.env.ENV_NAME,
+        version: "1.0.0",
+        message: `Hello from ${process.env.ENV_NAME}`,
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
